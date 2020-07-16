536029b8b73ad04a104660dc546713ef1fc1a1ca
ailinal <ailinal@yahoo.com>
2020-07-16 17:28:31 +0800

    up

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import pyautogui
 import time
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s  %(filename)s : %(levelname)s  %(message)s',
+                    datefmt='%Y-%m-%d %A %H:%M:%S')
+# vars
+timer = 0
+count = 0
 
 
 def click_button(filename, sleep=2, clear=True, grayscale=False, post_fun=None, args=None):
@@ -13,7 +18,8 @@ def click_button(filename, sleep=2, clear=True, grayscale=False, post_fun=None, 
 
     if _cor is None:
         return False
-    x, y = pyautogui.center(_cor)
+    else:
+        x, y = pyautogui.center(_cor)
     x = x * factor
     y = y * factor
     pyautogui.click(x, y)
@@ -21,20 +27,19 @@ def click_button(filename, sleep=2, clear=True, grayscale=False, post_fun=None, 
         timer = 0
     time.sleep(sleep)
     if post_fun is not None:
-        post_fun(args)
+        return post_fun(args)
 
 
 def wait_for(filename):
     counter = 0
     while True:
-        counter += 5
-        if counter > 20:
+        counter += 3
+        if counter > 60:
             break
         _cor = pyautogui.locateOnScreen(filename, confidence=0.8)
+        time.sleep(3)
         if _cor is not None:
             return True
-        else:
-            time.sleep(5)
 
 
 def _exit():
@@ -46,25 +51,26 @@ def _exit():
 
 click_button('icon.png')
 
-timer = 0
-count = 0
+
 while True:
     count += 1
+    # step 1
     logging.info('loop %d' % count)
     click_button('start_button.png', post_fun=wait_for, args='initial.png')
     click_button('confirm_button.png')
-
-    cor2 = pyautogui.locateOnScreen('complete.png', confidence=0.8, grayscale=True)
+    # step 2
+    cor2 = pyautogui.locateOnScreen(
+        'complete.png', confidence=0.8, grayscale=True)
     cor3 = pyautogui.locateOnScreen('continue_button.png', confidence=0.8)
     if cor3 is not None:
         if cor2 is not None:
             break
         else:
             click_button('continue_button.png')
-
-    cor = pyautogui.locateOnScreen('finish.png', confidence=0.8, grayscale=True)
-    cor1 = pyautogui.locateOnScreen('confirm_button.png', confidence=0.8, grayscale=True)
-    if (cor is not None) and (cor1 is None) and timer >= 10:
+    # step 3
+    cor = pyautogui.locateOnScreen(
+        'finish.png', confidence=0.8, grayscale=True)
+    if cor is not None:
         click_button('setting_button.png', sleep=1)
         click_button('concept_button.png')
     time.sleep(5)
