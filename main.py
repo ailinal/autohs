abf67eee2640e8d537d28b3c97db566068f2547b
ailinal <ailinal@yahoo.com>
2020-07-29 22:36:37 +0800

    up

--- main.py
+++ main.py
@@ -9,13 +9,21 @@ logging.basicConfig(level=logging.INFO,
 timer = 0
 count = 0
 
+# factor = 1440 / 2880
+factor = 1920 / 3840
+pre_path = 'resources/3840/'
 
-def click_button(filename, sleep=2, clear=True, grayscale=False, post_fun=None, args=None):
+
+def check_button(filename):
+    _cor = pyautogui.locateOnScreen(
+        pre_path + filename, confidence=0.9, grayscale=True)
+    return _cor
+
+
+def click_button(filename, sleep=2, clear=True, post_fun=None, args=None):
     logging.info('click %s' % filename)
     global timer
-    factor = 1440 / 2880
-    _cor = pyautogui.locateOnScreen(filename, confidence=0.9, grayscale=True)
-
+    _cor = check_button(filename)
     if _cor is None:
         return False
     else:
@@ -36,7 +44,7 @@ def wait_for(filename):
         counter += 3
         if counter > 60:
             break
-        _cor = pyautogui.locateOnScreen(filename, confidence=0.8)
+        _cor = check_button(filename)
         time.sleep(3)
         if _cor is not None:
             return True
@@ -51,7 +59,6 @@ def _exit():
 
 click_button('icon.png')
 
-
 while True:
     count += 1
     # step 1
@@ -59,17 +66,15 @@ while True:
     click_button('start_button.png', post_fun=wait_for, args='initial.png')
     click_button('confirm_button.png')
     # step 2
-    cor2 = pyautogui.locateOnScreen(
-        'complete.png', confidence=0.8, grayscale=True)
-    cor3 = pyautogui.locateOnScreen('continue_button.png', confidence=0.8)
+    cor2 = check_button('complete.png')
+    cor3 = check_button('continue_button.png')
     if cor3 is not None:
         if cor2 is not None:
             break
         else:
             click_button('continue_button.png')
     # step 3
-    cor = pyautogui.locateOnScreen(
-        'finish.png', confidence=0.8, grayscale=True)
+    cor = check_button('finish.png')
     if cor is not None:
         click_button('setting_button.png', sleep=1)
         click_button('concept_button.png')
