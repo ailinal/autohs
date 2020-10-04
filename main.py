e58e2962c745c34ada6964fce791646e8a8a87f1
ailinal <ailinal@yahoo.com>
2020-10-04 17:15:44 +0800

    update

--- main.py
+++ main.py
@@ -16,27 +16,31 @@ count = 0
 
 flag = True
 if args.e is True:
-    flag=False
+    flag = False
 
 # factor = 1440 / 2880
-# factor = 1920 / 3840
-factor = 1
-pre_path = 'resources/win/'
-cache_list=['setting_button.png', 'concept_button.png']
+factor = 1920 / 3840
+# factor = 1
+pre_path = 'resources/3840/'
+cache_list = ['setting_button.png', 'concept_button.png']
+
 
 def check_button(filename):
     _cor = pyautogui.locateOnScreen(
         pre_path + filename, confidence=0.9, grayscale=True)
     return _cor
 
+
 cache = {}
+
+
 def click_button(filename, sleep=2, clear=True, post_fun=None, args=None):
     global cache
     logging.info('click %s' % filename)
     global timer
 
     if filename in cache.keys():
-        x,y = cache[filename]
+        x, y = cache[filename]
         logging.info('cache hit')
     else:
         _cor = check_button(filename)
@@ -46,7 +50,7 @@ def click_button(filename, sleep=2, clear=True, post_fun=None, args=None):
         x = x * factor
         y = y * factor
         if filename in cache_list:
-            cache[filename] = (x,y)
+            cache[filename] = (x, y)
     pyautogui.click(x, y)
     if clear:
         timer = 0
@@ -83,17 +87,15 @@ while True:
     click_button('start_button.png', post_fun=wait_for, args='initial.png')
     click_button('confirm_button.png')
     # step 2
-  
+
     cor3 = check_button('continue_button.png')
     if cor3 is not None:
         if flag:
             cor2 = check_button('complete.png')
             if cor2 is not None:
-                break                
-            else:
-                click_button('continue_button.png')
-        else:
-            click_button('continue_button.png')
+                break
+        click_button('continue_button.png')
+    pyautogui.click()
     # step 3
     cor = check_button('finish.png')
     if cor is not None:
@@ -101,6 +103,5 @@ while True:
         click_button('concept_button.png')
     time.sleep(5)
     timer += 5
-    pyautogui.click()
 
 _exit()
